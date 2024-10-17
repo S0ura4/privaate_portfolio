0e00484fdec215fad2b6ad6f30cb0f0cec95a12a
Sourav <souravtowork@gmail.com>
2024-10-17 14:00:10 +0545

    updated images and names

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -6,6 +6,6 @@ addons.setConfig({
     ...themes.dark,
     brandImage: './icon.svg',
     brandTitle: 'SOURAV KARKI Components',
-    brandUrl: 'https://hamishw.com',
+    brandUrl: 'https://sourav.com',
   },
 });
